a32c4a9d80ce1852d94aa88c43f7787f9bc6bb40
Evgeny Alexeev <e.alexeev@yahoo.com>
2026-03-05 10:36:28 +0500

    Add macOS CI build workflow for xcodebuild validation

---
+++ Murmur/Settings/SettingsModel.swift
@@ -0,0 +1,82 @@
+import Foundation
+
+final class SettingsModel: ObservableObject {
+    static let shared = SettingsModel()
+
+    private enum Keys {
+        static let hotkeyKeyCode = "hotkeyKeyCode"
+        static let hotkeyModifiers = "hotkeyModifiers"
+        static let translationEnabled = "translationEnabled"
+        static let targetLanguage = "targetLanguage"
+        static let whisperBackend = "whisperBackend"
+        static let whisperModel = "whisperModel"
+        static let launchAtLogin = "launchAtLogin"
+    }
+
+    private enum Defaults {
+        static let hotkeyKeyCode = 49
+        static let hotkeyModifiers = 0x0008_0000
+        static let translationEnabled = false
+        static let targetLanguage = "en"
+        static let whisperBackend = "local"
+        static let whisperModel = "base"
+        static let launchAtLogin = false
+    }
+
+    private let userDefaults: UserDefaults
+
+    var launchAtLoginDidChange: ((Bool) -> Void)?
+
+    @Published var hotkeyKeyCode: Int {
+        didSet { userDefaults.set(hotkeyKeyCode, forKey: Keys.hotkeyKeyCode) }
+    }
+
+    @Published var hotkeyModifiers: Int {
+        didSet { userDefaults.set(hotkeyModifiers, forKey: Keys.hotkeyModifiers) }
+    }
+
+    @Published var translationEnabled: Bool {
+        didSet { userDefaults.set(translationEnabled, forKey: Keys.translationEnabled) }
+    }
+
+    @Published var targetLanguage: String {
+        didSet { userDefaults.set(targetLanguage, forKey: Keys.targetLanguage) }
+    }
+
+    @Published var whisperBackend: String {
+        didSet { userDefaults.set(whisperBackend, forKey: Keys.whisperBackend) }
+    }
+
+    @Published var whisperModel: String {
+        didSet { userDefaults.set(whisperModel, forKey: Keys.whisperModel) }
+    }
+
+    @Published var launchAtLogin: Bool {
+        didSet {
+            userDefaults.set(launchAtLogin, forKey: Keys.launchAtLogin)
+            launchAtLoginDidChange?(launchAtLogin)
+        }
+    }
+
+    private init(userDefaults: UserDefaults = .standard) {
+        self.userDefaults = userDefaults
+
+        hotkeyKeyCode = userDefaults.object(forKey: Keys.hotkeyKeyCode) as? Int ?? Defaults.hotkeyKeyCode
+        hotkeyModifiers = userDefaults.object(forKey: Keys.hotkeyModifiers) as? Int ?? Defaults.hotkeyModifiers
+        translationEnabled = userDefaults.object(forKey: Keys.translationEnabled) as? Bool ?? Defaults.translationEnabled
+        targetLanguage = userDefaults.string(forKey: Keys.targetLanguage) ?? Defaults.targetLanguage
+        whisperBackend = userDefaults.string(forKey: Keys.whisperBackend) ?? Defaults.whisperBackend
+        whisperModel = userDefaults.string(forKey: Keys.whisperModel) ?? Defaults.whisperModel
+        launchAtLogin = userDefaults.object(forKey: Keys.launchAtLogin) as? Bool ?? Defaults.launchAtLogin
+    }
+
+    func reset() {
+        hotkeyKeyCode = Defaults.hotkeyKeyCode
+        hotkeyModifiers = Defaults.hotkeyModifiers
+        translationEnabled = Defaults.translationEnabled
+        targetLanguage = Defaults.targetLanguage
+        whisperBackend = Defaults.whisperBackend
+        whisperModel = Defaults.whisperModel
+        launchAtLogin = Defaults.launchAtLogin
+    }
+}
